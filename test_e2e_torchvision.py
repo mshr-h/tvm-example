c1b66404eec6abe8e80af4e86ff244ab08b43670
Masahiro Hiramori <mhg00g13@gmail.com>
2025-04-25 17:52:46 +0900

    add quantized and segmentaion models

--- test_e2e_torchvision.py
+++ test_e2e_torchvision.py
@@ -8,7 +8,9 @@ import tvm.testing
 from tvm.relax.frontend.torch import from_exported_program
 
 
-def verify_model(torch_model, example_args, example_kwargs={}, target: str = "llvm", dev=tvm.cpu()):
+def verify_model(
+    torch_model, example_args, example_kwargs={}, target: str = "llvm", dev=tvm.cpu()
+):
     # PyTorch
     exported_program = export(torch_model, args=example_args, kwargs=example_kwargs)
     torch_output: torch.Tensor = exported_program.module()(*example_args)
@@ -62,88 +64,48 @@ def verify_torchvision_model(model_name):
     verify_model(model, example_args)
 
 
-def test_e2e_alexnet():
-    verify_torchvision_model("alexnet")
-
-
-def test_e2e_convnext_tiny():
-    verify_torchvision_model("convnext_tiny")
-
-
-def test_e2e_densenet121():
-    verify_torchvision_model("densenet121")
-
-
-def test_e2e_efficientnet_b0():
-    verify_torchvision_model("efficientnet_b0")
-
-
-def test_e2e_efficientnet_v2_s():
-    verify_torchvision_model("efficientnet_v2_s")
-
-
-def test_e2e_inception_v3():
-    verify_torchvision_model("inception_v3")
-
-
-def test_e2e_maxvit_t():
-    verify_torchvision_model("maxvit_t")
-
-
-def test_e2e_mnasnet0_5():
-    verify_torchvision_model("mnasnet0_5")
-
-
-def test_e2e_mobilenet_v2():
-    verify_torchvision_model("mobilenet_v2")
-
-
-def test_e2e_mobilenet_v3_small():
-    verify_torchvision_model("mobilenet_v3_small")
-
-
-def test_e2e_regnet_x_400mf():
-    verify_torchvision_model("regnet_x_400mf")
-
-
-def test_e2e_resnet18():
-    verify_torchvision_model("resnet18")
-
-
-def test_e2e_resnext50_32x4d():
-    verify_torchvision_model("resnext50_32x4d")
-
-
-def test_e2e_shufflenet_v2_x0_5():
-    verify_torchvision_model("shufflenet_v2_x0_5")
-
-
-def test_e2e_squeezenet1_0():
-    verify_torchvision_model("squeezenet1_0")
-
-
-def test_e2e_swin_t():
-    verify_torchvision_model("swin_t")
-
-
-def test_e2e_swin_v2_t():
-    verify_torchvision_model("swin_v2_t")
-
-
-def test_e2e_vgg11():
-    verify_torchvision_model("vgg11")
-
-
-def test_e2e_vgg11_bn():
-    verify_torchvision_model("vgg11_bn")
-
-
-def test_e2e_vit_b_32():
-    verify_torchvision_model("vit_b_32")
-
-
-def test_e2e_wide_resnet50_2():
-    verify_torchvision_model("wide_resnet50_2")
+@pytest.mark.parametrize(
+    "torch_model_name",
+    [
+        # classification models
+        "alexnet",
+        "convnext_tiny",
+        "densenet121",
+        "efficientnet_b0",
+        "efficientnet_v2_s",
+        "inception_v3",
+        "maxvit_t",
+        "mnasnet0_5",
+        "mobilenet_v2",
+        "mobilenet_v3_small",
+        "regnet_x_400mf",
+        "resnet18",
+        "resnext50_32x4d",
+        "shufflenet_v2_x0_5",
+        "squeezenet1_0",
+        "swin_t",
+        "swin_v2_t",
+        "vgg11",
+        "vgg11_bn",
+        "vit_b_32",
+        "wide_resnet50_2",
+        # quantized models
+        "quantized_googlenet",
+        "quantized_inception_v3",
+        "quantized_mobilenet_v2",
+        "quantized_mobilenet_v3_large",
+        "quantized_resnet18",
+        "quantized_resnext101_32x8d",
+        "quantized_shufflenet_v2_x0_5",
+        # segmentation models
+        "deeplabv3_mobilenet_v3_large",
+        "deeplabv3_resnet50",
+        "fcn_resnet50",
+        "lraspp_mobilenet_v3_large",
+    ],
+)
+def test_e2e(torch_model_name: str):
+    verify_torchvision_model(torch_model_name)
 
 
 if __name__ == "__main__":
