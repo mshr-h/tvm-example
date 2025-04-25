ec9b09690cefb593394aa3ea236ef0c70a9169a6
Masahiro Hiramori <mhg00g13@gmail.com>
2025-04-25 17:53:17 +0900

    add sam2

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # exportedprogram-to-tvm-relax
+
 Collections of PyTorch ExportedProgram to TVM Relax translation example.
+
+## Prerequisite
+
+- [uv](https://docs.astral.sh/uv/)
+- llvm
+
+## prepare venv and install tvm, pytorch
+
+```bash
+uv venv
+source .venv/bin/activate
+uv pip install cmake ninja setuptools cython pytest
+uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+cd 3rdparty
+./build-tvm.sh --clean --llvm llvm-config # change if you want to use different llvm version
+```
+
+## (wip) sam2
+
+```bash
+cd test_sam2
+git clone https://github.com/facebookresearch/sam2.git sam2_repo && cd sam2_repo
+uv pip install -e .
+cd checkpoints/
+bash download_ckpts.sh
+cd ..
+```
+
+- `AssertionError: Unsupported function types ['sym_size.int', 'upsample_bicubic2d.vec', 'mul']`
---
+++ test_sam2/test_sam2.py
@@ -0,0 +1,51 @@
+import torch
+from sam2.build_sam import build_sam2
+from sam2.sam2_image_predictor import SAM2ImagePredictor
+from torch.export import Dim
+
+import tvm
+
+from tvm import relax
+
+from tvm.relax.frontend.torch import from_exported_program
+
+def main():
+    sam2_checkpoint = "./sam2_repo/checkpoints/sam2.1_hiera_large.pt"
+    model_cfg = "configs/sam2.1/sam2.1_hiera_l.yaml"
+    device = "cpu"
+
+    sam2_model = build_sam2(model_cfg, sam2_checkpoint, device=device)
+
+    predictor = SAM2ImagePredictor(sam2_model)
+    # torch_model = predictor.model.image_encoder.eval()
+    torch_model = predictor.model.image_encoder.bfloat16().eval()
+    name = "sam2.1_hiera_large_exported"
+    img_size=1024
+
+    "Save the model to pytorch ExportedProgram format."
+
+    example_args = (
+        torch.randn(5, 3, img_size, img_size).to(device).type(torch.bfloat16)
+    )
+
+    # dynamic shapes
+    batch_size = Dim("batch", min=2, max=20)
+    # height = Dim("height", min=2, max=2048)
+    # width = Dim("width", min=2, max=2048)
+    dynamic_shapes = {
+        "sample": {0: batch_size},
+    }
+
+    with torch.no_grad():
+        ep_module = torch.export.export(
+            torch_model,
+            (example_args,),
+            dynamic_shapes=dynamic_shapes,
+            strict=False,
+        )
+
+    mod = from_exported_program(ep_module)
+
+
+if __name__ == "__main__":
+    main()
