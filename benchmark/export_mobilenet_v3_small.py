65d608d53023e14c7bdfab906fc6a167fd9aaa37
Masahiro Hiramori <mhg00g13@gmail.com>
2025-12-04 15:23:24 +0900

    accept cli args

--- benchmark/export_mobilenet_v3_small.py
+++ benchmark/export_mobilenet_v3_small.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 import torchvision
 import tvm
@@ -5,7 +7,26 @@ from tvm.relax.frontend.torch import from_exported_program
 
 
 def main():
-    tvm_device = tvm.cpu()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--output",
+        type=str,
+        default="mobilenet_v3_small.so",
+        help="Path to save the compiled Relax module",
+    )
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="cpu",
+        choices=["cpu", "cuda"],
+        help="Device to compile for",
+    )
+    args = parser.parse_args()
+
+    if args.device == "cpu":
+        tvm_device = tvm.cpu()
+    else:
+        tvm_device = tvm.cuda()
     tvm_target = tvm.target.Target.from_device(tvm_device)
 
     model = torchvision.models.mobilenet_v3_small(weights="IMAGENET1K_V1").eval()
@@ -15,7 +36,8 @@ def main():
     mod = from_exported_program(exported_program)
     mod = tvm.relax.transform.DecomposeOpsForInference()(mod)
     exe = tvm.compile(mod, target=tvm_target)
-    exe.export_library("mobilenet_v3_small.so")
+    exe.export_library(args.output)
+
 
 if __name__ == "__main__":
     main()
